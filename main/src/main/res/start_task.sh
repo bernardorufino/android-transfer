8464235e72210361f4209ed9003cdc4db2da4324
Bernardo Rufino <brufino@google.com>
2019-03-15 14:31:56 -0300

    Add possible time formula to bash script

--- main/src/main/res/start_task.sh
+++ main/src/main/res/start_task.sh
@@ -9,6 +9,8 @@ consumer_interval=$6
 consumer_buffer=$7
 repeat=${8-1}
 
+# C + max(Pd / Pc * Pi, Pd / min(Pc, Tb) * max(1, min(Pc, Tb) / Cb) * Ci)
+
 echo "Submitted $repeat $task $producer_data $producer_interval $producer_chunk $transfer_buffer $consumer_interval $consumer_buffer"
 # We explicitly set stdin to /dev/null because otherwise it captures stdin from start_tasks.sh
 adb shell am broadcast \
